7f4129ee2a80f37ab5716ac7c9f54edc07ac58ac
sneha-advani <sneha.d.advani@gmail.com>
2017-04-09 13:30:27 -0400

    IT WORKS NOW

---
+++ densityData.js
@@ -0,0 +1,198 @@
+var data = [{
+code:'AFG',country:'Afghanistan',density:47},{
+code:'ALB',country:'Albania',density:99},{
+code:'DZA',country:'Algeria',density:92},{
+code:'AND',country:'Andorra',density:0},{
+code:'AGO',country:'Angola',density:73},{
+code:'ATG',country:'Antigua and Barbuda',density:0},{
+code:'ARG',country:'Argentina',density:99},{
+code:'ARM',country:'Armenia',density:100},{
+code:'AUS',country:'Australia',density:0},{
+code:'AUT',country:'Austria',density:0},{
+code:'AZE',country:'Azerbaijan',density:100},{
+code:'BHS',country:'Bahamas',density:0},{
+code:'BHR',country:'Bahrain',density:98},{
+code:'BGD',country:'Bangladesh',density:81},{
+code:'BRB',country:'Barbados',density:0},{
+code:'BLR',country:'Belarus',density:100},{
+code:'BEL',country:'Belgium',density:0},{
+code:'BLZ',country:'Belize',density:0},{
+code:'BEN',country:'Benin',density:42},{
+code:'BTN',country:'Bhutan',density:74},{
+code:'BOL',country:'Bolivia (Plurinational State of)',density:99},{
+code:'BIH',country:'Bosnia and Herzegovina',density:100},{
+code:'BWA',country:'Botswana',density:98},{
+code:'BRA',country:'Brazil',density:98},{
+code:'BRN',country:'Brunei Darussalam',density:99},{
+code:'BGR',country:'Bulgaria',density:98},{
+code:'BFA',country:'Burkina Faso',density:39},{
+code:'BDI',country:'Burundi',density:89},{
+code:'CPV',country:'Cabo Verde',density:98},{
+code:'KHM',country:'Cambodia',density:87},{
+code:'CMR',country:'Cameroon',density:81},{
+code:'CAN',country:'Canada',density:0},{
+code:'CAF',country:'Central African Republic',density:36},{
+code:'TCD',country:'Chad',density:50},{
+code:'CHL',country:'Chile',density:99},{
+code:'CHN',country:'China',density:100},{
+code:'COL',country:'Colombia',density:98},{
+code:'COM',country:'Comoros',density:87},{
+code:'COG',country:'Congo',density:81},{
+code:'COK',country:'Cook Islands',density:0},{
+code:'CRI',country:'Costa Rica',density:99},{
+code:'CIV',country:'Côte dIvoire',density:48},{
+code:'HRV',country:'Croatia',density:100},{
+code:'CUB',country:'Cuba',density:100},{
+code:'CYP',country:'Cyprus',density:100},{
+code:'CZE',country:'Czech Republic',density:0},{
+code:'PRK',country:'Democratic Peoples Republic of Korea',density:100},{
+code:'COD',country:'Democratic Republic of the Congo',density:84},{
+code:'DNK',country:'Denmark',density:0},{
+code:'DJI',country:'Djibouti',density:0},{
+code:'DMA',country:'Dominica',density:0},{
+code:'DOM',country:'Dominican Republic',density:97},{
+code:'ECU',country:'Ecuador',density:99},{
+code:'EGY',country:'Egypt',density:88},{
+code:'SLV',country:'El Salvador',density:96},{
+code:'GNQ',country:'Equatorial Guinea',density:98},{
+code:'ERI',country:'Eritrea',density:92},{
+code:'EST',country:'Estonia',density:100},{
+code:'ETH',country:'Ethiopia',density:55},{
+code:'FJI',country:'Fiji',density:0},{
+code:'FIN',country:'Finland',density:0},{
+code:'FRA',country:'France',density:0},{
+code:'GAB',country:'Gabon',density:89},{
+code:'GMB',country:'Gambia',density:71},{
+code:'GEO',country:'Georgia',density:100},{
+code:'DEU',country:'Germany',density:0},{
+code:'GHA',country:'Ghana',density:86},{
+code:'GRC',country:'Greece',density:99},{
+code:'GRD',country:'Grenada',density:0},{
+code:'GTM',country:'Guatemala',density:94},{
+code:'GIN',country:'Guinea',density:31},{
+code:'GNB',country:'Guinea-Bissau',density:75},{
+code:'GUY',country:'Guyana',density:93},{
+code:'HTI',country:'Haiti',density:72},{
+code:'VAT',country:'Holy See',density:0},{
+code:'HND',country:'Honduras',density:95},{
+code:'HUN',country:'Hungary',density:99},{
+code:'ISL',country:'Iceland',density:0},{
+code:'IND',country:'India',density:86},{
+code:'IDN',country:'Indonesia',density:99},{
+code:'IRN',country:'Iran (Islamic Republic of)',density:98},{
+code:'IRQ',country:'Iraq',density:82},{
+code:'IRL',country:'Ireland',density:0},{
+code:'ISR',country:'Israel',density:0},{
+code:'ITA',country:'Italy',density:100},{
+code:'JAM',country:'Jamaica',density:96},{
+code:'JPN',country:'Japan',density:0},{
+code:'JOR',country:'Jordan',density:99},{
+code:'KAZ',country:'Kazakhstan',density:100},{
+code:'KEN',country:'Kenya',density:82},{
+code:'KIR',country:'Kiribati',density:0},{
+code:'KWT',country:'Kuwait',density:99},{
+code:'KGZ',country:'Kyrgyzstan',density:100},{
+code:'LAO',country:'Lao Peoples Democratic Republic',density:84},{
+code:'LVA',country:'Latvia',density:100},{
+code:'LBN',country:'Lebanon',density:99},{
+code:'LSO',country:'Lesotho',density:83},{
+code:'LBR',country:'Liberia',density:49},{
+code:'LBY',country:'Libya',density:100},{
+code:'LIE',country:'Liechtenstein',density:0},{
+code:'LTU',country:'Lithuania',density:100},{
+code:'LUX',country:'Luxembourg',density:0},{
+code:'MDG',country:'Madagascar',density:65},{
+code:'MWI',country:'Malawi',density:72},{
+code:'MYS',country:'Malaysia',density:98},{
+code:'MDV',country:'Maldives',density:99},{
+code:'MLI',country:'Mali',density:44},{
+code:'MLT',country:'Malta',density:99},{
+code:'MHL',country:'Marshall Islands',density:0},{
+code:'MRT',country:'Mauritania',density:56},{
+code:'MUS',country:'Mauritius',density:98},{
+code:'MEX',country:'Mexico',density:98},{
+code:'FSM',country:'Micronesia (Federated States of)',density:0},{
+code:'MCO',country:'Monaco',density:0},{
+code:'MNG',country:'Mongolia',density:98},{
+code:'MNE',country:'Montenegro',density:99},{
+code:'MAR',country:'Morocco',density:82},{
+code:'MOZ',country:'Mozambique',density:67},{
+code:'MMR',country:'Myanmar',density:96},{
+code:'NAM',country:'Namibia',density:87},{
+code:'NRU',country:'Nauru',density:0},{
+code:'NPL',country:'Nepal',density:85},{
+code:'NLD',country:'Netherlands',density:0},{
+code:'NZL',country:'New Zealand',density:0},{
+code:'NIC',country:'Nicaragua',density:87},{
+code:'NER',country:'Niger',density:24},{
+code:'NGA',country:'Nigeria',density:66},{
+code:'NIU',country:'Niue',density:0},{
+code:'NOR',country:'Norway',density:0},{
+code:'OMN',country:'Oman',density:98},{
+code:'PAK',country:'Pakistan',density:71},{
+code:'PLW',country:'Palau',density:100},{
+code:'PAN',country:'Panama',density:98},{
+code:'PNG',country:'Papua New Guinea',density:72},{
+code:'PRY',country:'Paraguay',density:99},{
+code:'PER',country:'Peru',density:99},{
+code:'PHL',country:'Philippines',density:98},{
+code:'POL',country:'Poland',density:100},{
+code:'PRT',country:'Portugal',density:99},{
+code:'QAT',country:'Qatar',density:97},{
+code:'KOR',country:'Republic of Korea',density:0},{
+code:'MDA',country:'Republic of Moldova',density:100},{
+code:'ROU',country:'Romania',density:99},{
+code:'RUS',country:'Russian Federation',density:100},{
+code:'RWA',country:'Rwanda',density:77},{
+code:'KNA',country:'Saint Kitts and Nevis',density:0},{
+code:'LCA',country:'Saint Lucia',density:0},{
+code:'VCT',country:'Saint Vincent and the Grenadines',density:0},{
+code:'WSM',country:'Samoa',density:99},{
+code:'SMR',country:'San Marino',density:0},{
+code:'STP',country:'Sao Tome and Principe',density:80},{
+code:'SAU',country:'Saudi Arabia',density:99},{
+code:'SEN',country:'Senegal',density:66},{
+code:'SRB',country:'Serbia',density:99},{
+code:'SYC',country:'Seychelles',density:99},{
+code:'SLE',country:'Sierra Leone',density:64},{
+code:'SGP',country:'Singapore',density:100},{
+code:'SVK',country:'Slovakia',density:0},{
+code:'SVN',country:'Slovenia',density:100},{
+code:'SLB',country:'Solomon Islands',density:0},{
+code:'SOM',country:'Somalia',density:0},{
+code:'ZAF',country:'South Africa',density:99},{
+code:'SSD',country:'South Sudan',density:37},{
+code:'ESP',country:'Spain',density:100},{
+code:'LKA',country:'Sri Lanka',density:98},{
+code:'PSE',country:'State of Palestine',density:99},{
+code:'SDN',country:'Sudan',density:88},{
+code:'SUR',country:'Suriname',density:98},{
+code:'SWZ',country:'Swaziland',density:94},{
+code:'SWE',country:'Sweden',density:0},{
+code:'CHE',country:'Switzerland',density:0},{
+code:'SYR',country:'Syrian Arab Republic',density:96},{
+code:'TJK',country:'Tajikistan',density:100},{
+code:'THA',country:'Thailand',density:97},{
+code:'MKD',country:'The former Yugoslav Republic of Macedonia',density:99},{
+code:'TLS',country:'Timor0Leste',density:80},{
+code:'TGO',country:'Togo',density:80},{
+code:'TON',country:'Tonga',density:99},{
+code:'TTO',country:'Trinidad and Tobago',density:100},{
+code:'TUN',country:'Tunisia',density:97},{
+code:'TUR',country:'Turkey',density:98},{
+code:'TKM',country:'Turkmenistan',density:100},{
+code:'TUV',country:'Tuvalu',density:0},{
+code:'UGA',country:'Uganda',density:87},{
+code:'UKR',country:'Ukraine',density:100},{
+code:'ARE',country:'United Arab Emirates',density:95},{
+code:'GBR',country:'United Kingdom',density:0},{
+code:'TZA',country:'United Republic of Tanzania',density:75},{
+code:'USA',country:'United States',density:0},{
+code:'URY',country:'Uruguay',density:99},{
+code:'UZB',country:'Uzbekistan',density:100},{
+code:'VUT',country:'Vanuatu',density:95},{
+code:'VEN',country:'Venezuela (Bolivarian Republic of)',density:98},{
+code:'VNM',country:'Viet Nam',density:97},{
+code:'YEM',country:'Yemen',density:88},{
+code:'ZMB',country:'Zambia',density:64},{
+code:'ZWE',country:'Zimbabwe',density:91}]
